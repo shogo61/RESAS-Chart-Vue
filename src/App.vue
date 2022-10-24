9b506a53ac29f3d2c7d53bbdb359fbaae847b414
shogo61 <shogo.611.lol@gmail.com>
2022-10-24 16:08:42 +0900

    displayChart

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,83 @@
 <template>
-  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   <div id="app">
     <h1>都道府県別人口推移</h1>
-    <div class="prefectures">
-      <getAPI></getAPI>
-
+    <div class="prefectures-area">
+			<getAPI @onAddSeries="addSeries" />
     </div>
+		<div id="chart-area">
+			<Highcharts :options="options" />
+		</div>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
 import getAPI from './components/getAPI.vue'
+import Highcharts from '../node_modules/highcharts'
 
 export default {
   name: 'App',
   components: {
-    getAPI
-  }
+		Highcharts: Highcharts,
+		getAPI: getAPI,
+  },
+	data(){
+		return {
+      // Highchartsの処理
+      options: {
+        // seriesにオブジェクトを追加するとグラフに描画される
+        series: [],
+        title: {
+          style: {
+            display: "none"
+          }
+        },
+        legend: {
+          align: "right",
+          verticalAlign: "top",
+          layout: "vertical"
+        },
+        xAxis: {
+          title: {
+            text: "年度"
+          },
+          categories: [
+            1960,
+            1965,
+            1970,
+            1975,
+            1980,
+            1985,
+            1990,
+            1995,
+            2000,
+            2005,
+            2010,
+            2015,
+          ]
+        },
+        yAxis: {
+          title: {
+            text: "人口数"
+          }
+        }
+      }
+    };
+  },
+	methods:{
+		drawChart: async function(options){
+			Highcharts.chart("chart-area",options)
+		},
+		addSeries: async function(population,name){
+			console.log(population)
+			this.options.series.push({
+				name: name,
+				data: population
+			});
+			this.drawChart(this.options);
+			console.log(this.options)
+			console.log("")
+		}
+	}
 }
 </script>
 
--- src/components/getAPI.vue
+++ src/components/getAPI.vue
@@ -1,58 +1,46 @@
-<!-- <template>
-  <div id="prefectures">
-    <ul>
-      <li v-for="prefecture in prefectures" :key="prefecrure.id">
-        <input type="checkbox">
-        {{prefecture}}
-      </li>
-    </ul>
-  </div>
-</template> -->
 <template>
-  <div class="prefectures-area">
-		{{selectedPrefectures}}
-    <div
-      v-for="prefecture in prefectures"
-      :key="prefecture.id"
-      class="prefecture"
-    >
-		<label>
-			<input
-			type="checkbox"
-			:id="prefecture.id"
-			:checked="prefecture.isChecked"
-			:for="prefecture.id"
-			@change="changed(prefecture)"
-			/>
-				{{prefecture.id}}
-        {{prefecture.name}}
-      </label>
-    </div>
-		<div id="chart-area">
-			{{getPopulation(selectedPrefectures)}}
-		</div>
+  <!-- <div class="prefectures-area"> -->
+  {{ this.population }}
+  <div
+    v-for="prefecture in prefectures"
+    :key="prefecture.id"
+    class="prefecture"
+  >
+    <label>
+      <input
+        type="checkbox"
+        :id="prefecture.id"
+        :checked="prefecture.isChecked"
+        :for="prefecture.id"
+        @change="changed(prefecture)"
+      />
+      {{ prefecture.id }}
+      {{ prefecture.name }}
+    </label>
   </div>
+	<div id="chart-area">
+
+	</div>
+  <!-- </div> -->
 </template>
 
-<!-- <script src="https://unpkg.com/axios@0.25.0/dist/axios.min.js"></script> -->
 <script>
 import axios from "axios";
-// import Highcharts from "https://code.highcharts.com/es-modules/masters/highcharts.src.js";
 
 const ACCESS_TOKEN = "Ea3RqjQjtf8O2XxJHuVlzUJLSxoClMzDpDSwg1IO";
 // var cnt = 0;
 
 export default {
-	data() {
-		return {
-			prefectures: [],
-			populationData: [],
-			selectedPrefectures: []
+  data() {
+    return {
+      prefectures: [],
+      // population: [],
+      selectedPrefectures: [],
     };
   },
+	emits:["onAddSeries"],
   mounted() {
     this.getPrefectures();
-    this.displayChart();
   },
   methods: {
     fetchAPI: function (path) {
@@ -68,8 +56,8 @@ export default {
       //asyncとawaitを付けないとresponseのresult以降を読み込まない
       const path = "prefectures";
       const response = await this.fetchAPI(path);
-      console.log(response)
-			// var id;
+      console.log(response);
+      // var id;
       this.prefectures = response.data.result.map((val) => {
         // valにprefCode,prefNameを格納する
         return {
@@ -78,39 +66,34 @@ export default {
           isChecked: false,
         };
       });
-			// var id = prefecrure.id
-      console.log(this.prefectures)
+      console.log(this.prefectures);
     },
-		// チェックボックスがクリックされた時の処理
-		// チェックがついたらselectedPrefectureに追加し、外れたらspliceで削除する
-		changed(prefecture){
-			prefecture.isChecked = !prefecture.isChecked
-			console.log(prefecture)
-			if(prefecture.isChecked){
-				let val = {id: prefecture.id, name:prefecture.name};
-				this.selectedPrefectures.push(val);
-			}else{
-				//prefecture.nameのインデックスを検索して削除
-				this.selectedPrefectures.splice(this.selectedPrefectures.indexOf(prefecture.name),1)
-			}
-		},
-		// 各年の人口を取得
-    getPopulation: async function(selectedPrefecture){
-			for(var i=0;i<this.selectedPrefectures.length;i++){
-				this.populationData.splice(0);
-				const path = `population/composition/perYear?&prefCode=${selectedPrefecture[i].id}`;
-				const response = await this.fetchAPI(path);
-				console.log(response);
-			// cnt ++;
-			// console.log(cnt)
-				
-				// console.log(response)
-				for(var j=0;j<8;j++){
-					this.populationData.push(response.data.result.data[0].data[j].value)
-				}
-			}
-			console.log(this.populationData)
-		}
+    // チェックボックスがクリックされた時の処理
+    // チェックがついたらselectedPrefectureに追加し、外れたらspliceで削除する
+    changed(prefecture) {
+      prefecture.isChecked = !prefecture.isChecked;
+      console.log(prefecture);
+      if (prefecture.isChecked) {
+        this.getPopulation(prefecture);
+      } else {
+        //prefecture.nameのインデックスを検索して削除
+        this.selectedPrefectures.splice(
+          this.selectedPrefectures.indexOf(prefecture.name)
+        );
+        //removeChart();
+      }
+    },
+    // 各年の人口を取得
+    getPopulation: async function (prefecture) {
+
+			const population = [];
+      const path = `population/composition/perYear?&prefCode=${prefecture.id}`;
+      const response = await this.fetchAPI(path);
+      for (var i = 0; i < 12; i++) {
+				population.push(response.data.result.data[0].data[i].value);
+      }
+      this.$emit("onAddSeries", population, prefecture.name);
+    },
   },
 };
 </script>
